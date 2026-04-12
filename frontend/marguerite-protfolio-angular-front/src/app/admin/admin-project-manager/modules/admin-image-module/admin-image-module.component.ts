a0bc232963b412ebdf70cbba5063cc32eec1b884
Antoine PRONNIER <antoine.pronnier@gmail.com>
2026-04-12 11:24:03 +0200

    Done single image module

--- frontend/marguerite-protfolio-angular-front/src/app/admin/admin-project-manager/modules/admin-image-module/admin-image-module.component.ts
+++ frontend/marguerite-protfolio-angular-front/src/app/admin/admin-project-manager/modules/admin-image-module/admin-image-module.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {ModuleComponent} from "../module.component";
 import {SingleImageModule} from "../../../../../services/projects/modules/single-image-module";
 import {ProjectMediaDto, ProjectMediaType} from "../../../../../dtos/projects/project-media-dto";
+import ProjectsMediasService from "../../../../../services/projects/projects-medias-service";
 
 @Component({
   selector: 'app-admin-image-module',
@@ -39,7 +40,58 @@ export class AdminImageModuleComponent extends ModuleComponent<SingleImageModule
       this.image = media
       this.module.imageId = media.id
       this.module.altImage = media.mediaDescription
+
+      this.determineImageOrientation()
+      this.alignCenterImage()
     })
+  }
+
+  alignCenterImage() {
+    if (!this.module) return;
+
+    this.module.imgCenter = true
+    this.module.imgLeft = false
+    this.module.imgRight = false
+  }
+
+  alignRightImage() {
+    if (!this.module) return;
+
+    this.module.imgCenter = false
+    this.module.imgLeft = false
+    this.module.imgRight = true
+  }
+
+  alignLeftImage() {
+    if (!this.module) return;
+
+    this.module.imgCenter = false
+    this.module.imgLeft = true
+    this.module.imgRight = false
+  }
+
+  /**
+   * Load the image and calculate if the image is in portrait or landscape mode
+   * @private
+   */
+  private determineImageOrientation() {
+    if (!this.module?.imageId) return;
+
+    const imageUrl: string = ProjectsMediasService.getMediaUrl(this.module.imageId)
+    const img = new Image();
+
+    img.onload = () => {
+      if (!this.module) return
+      this.module.imgPortrait = img.naturalHeight > img.naturalWidth;
+    };
+
+    img.onerror = () => {
+      if (!this.module) return
+      this.module.imgPortrait = false;
+      alert("Impossible de charger l'image pour détecter son orientation.");
+    };
+
+    img.src = imageUrl;
   }
 
 }
